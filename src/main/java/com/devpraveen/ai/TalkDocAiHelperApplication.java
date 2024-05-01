272676a9ca4fc0c7dd8d99c8de6d621aba2e6279
dev-praveen <spraveen0244@gmail.com>
2024-05-01 10:46:48 +0530

    project setup

---
+++ src/main/java/com/devpraveen/ai/TalkDocAiHelperApplication.java
@@ -0,0 +1,12 @@
+package com.devpraveen.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkDocAiHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TalkDocAiHelperApplication.class, args);
+  }
+}
